153059cbda2adb6b35413cff8a56804862839a5d
MohamedKhalilAssa <assaddiki.mohamed1@gmail.com>
2025-04-15 16:55:21 +0100

    First commit

---
+++ back-end/src/main/java/ma/inpt/cedoc/CedocApplication.java
@@ -0,0 +1,13 @@
+package ma.inpt.cedoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CedocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CedocApplication.class, args);
+	}
+
+}
