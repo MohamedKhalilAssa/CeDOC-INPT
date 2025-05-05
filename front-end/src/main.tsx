2405a20c09a4a186216c4bf251acf2be9e6f0de2
MohamedKhalilAssa <assaddiki.mohamed1@gmail.com>
2025-05-05 01:33:46 +0100

    cleaning front

--- front-end/src/main.tsx
+++ front-end/src/main.tsx
@@ -1,7 +1,7 @@
+import App from '@/App'
 import '@/assets/index.css'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
