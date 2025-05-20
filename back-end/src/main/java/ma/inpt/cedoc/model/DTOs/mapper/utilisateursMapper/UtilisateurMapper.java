1985a3c7499e912ae7e7e83aaee5bece98cd5bd0
MohamedKhalilAssa <assaddiki.mohamed1@gmail.com>
2025-05-20 23:05:18 +0100

    mapper

--- back-end/src/main/java/ma/inpt/cedoc/model/DTOs/mapper/utilisateursMapper/UtilisateurMapper.java
+++ back-end/src/main/java/ma/inpt/cedoc/model/DTOs/mapper/utilisateursMapper/UtilisateurMapper.java
@@ -2,18 +2,10 @@ package ma.inpt.cedoc.model.DTOs.mapper.utilisateursMapper;
 
 import java.util.List;
 
-import org.mapstruct.InjectionStrategy;
-import org.mapstruct.Mapper;
-
 import ma.inpt.cedoc.model.DTOs.Utilisateurs.*;
 import ma.inpt.cedoc.model.DTOs.auth.RegisterRequestDTO;
-import ma.inpt.cedoc.model.DTOs.mapper.MapperHelpers.utilisateurs.LieuDeNaissanceMapperHelper;
-import ma.inpt.cedoc.model.DTOs.mapper.MapperHelpers.utilisateurs.NationaliteMapperHelper;
 import ma.inpt.cedoc.model.entities.utilisateurs.*;
 
-@Mapper(componentModel = "spring", injectionStrategy = InjectionStrategy.CONSTRUCTOR, uses = {
-                NationaliteMapperHelper.class,
-                LieuDeNaissanceMapperHelper.class }, unmappedTargetPolicy = org.mapstruct.ReportingPolicy.IGNORE)
 public interface UtilisateurMapper {
     Utilisateur fromRequestDTO(UtilisateurRequestDTO dto);
     Utilisateur fromRegisterDTO(RegisterRequestDTO dto);
