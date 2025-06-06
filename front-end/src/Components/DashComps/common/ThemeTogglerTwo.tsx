5047941373ef0908a01037d625897e9373711875
MohamedKhalilAssa <assaddiki.mohamed1@gmail.com>
2025-06-06 16:01:21 +0100

    Structure fix

--- front-end/src/Components/DashComps/common/ThemeTogglerTwo.tsx
+++ front-end/src/Components/DashComps/common/ThemeTogglerTwo.tsx
@@ -1,4 +1,4 @@
-import { useTheme } from "@/Context/Auth/DashContext/ThemeContext";
+import { useTheme } from "@/Context/DashContext/ThemeContext";
 
 export default function ThemeTogglerTwo() {
   const { toggleTheme } = useTheme();
