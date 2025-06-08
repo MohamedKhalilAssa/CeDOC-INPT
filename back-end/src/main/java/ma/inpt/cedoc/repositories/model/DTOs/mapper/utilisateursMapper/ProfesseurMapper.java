65d4a49dc1c32205efd838fe65321b3f1c6076cf
MohamedKhalilAssa <assaddiki.mohamed1@gmail.com>
2025-06-08 14:34:05 +0100

    fix

--- back-end/src/main/java/ma/inpt/cedoc/repositories/model/DTOs/mapper/utilisateursMapper/ProfesseurMapper.java
+++ back-end/src/main/java/ma/inpt/cedoc/repositories/model/DTOs/mapper/utilisateursMapper/ProfesseurMapper.java
@@ -1,8 +1,14 @@
 package ma.inpt.cedoc.repositories.model.DTOs.mapper.utilisateursMapper;
 
+import java.util.List;
+
 import ma.inpt.cedoc.repositories.model.DTOs.Utilisateurs.simpleDTOs.ProfesseurResponseDTO;
 import ma.inpt.cedoc.repositories.model.entities.utilisateurs.Professeur;
 
 public interface ProfesseurMapper {
     ProfesseurResponseDTO toSimpleDTO(Professeur p);
+
+    ProfesseurResponseDTO toDto(Professeur p);
+
+    List<ProfesseurResponseDTO> toDtoList(List<Professeur> professeurs);
 }
